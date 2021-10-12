6b55ab8face81c1d05b34a42c5503c256912bd0e
hieyou1 <31020711+hieyou1@users.noreply.github.com>
2021-10-11 20:30:15 -0500

    add ? and .

--- src/client.js
+++ src/client.js
@@ -2,8 +2,8 @@ import copyToClipboard from "copy-to-clipboard";
 const id = (eid) => {
     return document.getElementById(eid);
 };
-const small = "abcdefghijklmnopqrstuvwxyz1234567890-*/+!".split("");
-const big = ":regional_indicator_a: :regional_indicator_b: :regional_indicator_c: :regional_indicator_d: :regional_indicator_e: :regional_indicator_f: :regional_indicator_g: :regional_indicator_h: :regional_indicator_i: :regional_indicator_j: :regional_indicator_k: :regional_indicator_l: :regional_indicator_m: :regional_indicator_n: :regional_indicator_o: :regional_indicator_p: :regional_indicator_q: :regional_indicator_r: :regional_indicator_s: :regional_indicator_t: :regional_indicator_u: :regional_indicator_v: :regional_indicator_w: :regional_indicator_x: :regional_indicator_y: :regional_indicator_z: :one: :two: :three: :four: :five: :six: :seven: :eight: :nine: :zero: :heavy_minus_sign: :heavy_multiplication_x: :heavy_division_sign: :heavy_plus_sign: :exclamation:".split(" ");
+const small = "abcdefghijklmnopqrstuvwxyz1234567890-*/+!?.".split("");
+const big = ":regional_indicator_a: :regional_indicator_b: :regional_indicator_c: :regional_indicator_d: :regional_indicator_e: :regional_indicator_f: :regional_indicator_g: :regional_indicator_h: :regional_indicator_i: :regional_indicator_j: :regional_indicator_k: :regional_indicator_l: :regional_indicator_m: :regional_indicator_n: :regional_indicator_o: :regional_indicator_p: :regional_indicator_q: :regional_indicator_r: :regional_indicator_s: :regional_indicator_t: :regional_indicator_u: :regional_indicator_v: :regional_indicator_w: :regional_indicator_x: :regional_indicator_y: :regional_indicator_z: :one: :two: :three: :four: :five: :six: :seven: :eight: :nine: :zero: :heavy_minus_sign: :heavy_multiplication_x: :heavy_division_sign: :heavy_plus_sign: :exclamation: :question: :black_circle:".split(" ");
 
 window.onload = async () => {
     id("small").onkeyup = id("small").onpaste = () => {
